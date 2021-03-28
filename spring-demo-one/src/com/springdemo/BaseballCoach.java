e7b3add592626553d484bb4fcc1fcb2cca7f5dcd
swara12262 <swaranjalisawant@gmail.com>
2021-03-28 15:47:52 +0530

    Rough demo

---
+++ spring-demo-one/src/com/springdemo/BaseballCoach.java
@@ -0,0 +1,11 @@
+package com.springdemo;
+
+public class BaseballCoach implements Coach{
+	
+	@Override
+	public String getDailyWorkout() {
+		return "spend 30 mins on batting pratice";
+		
+	}
+
+}
---
+++ spring-demo-one/src/com/springdemo/MyApp.java
@@ -0,0 +1,15 @@
+package com.springdemo;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+		
+		//create new object
+		Coach b=  new BaseballCoach();
+		
+		//use new object
+		System.out.println(b.getDailyWorkout());
+
+	}
+
+}
