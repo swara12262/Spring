e5e06881542bcf6124005c842dd1516bda957296
swara12262 <swaranjalisawant@gmail.com>
2021-03-30 08:39:56 +0530

    exercise

---
+++ spring-demo-one/src/com/springdemo/CricketCoach.java
@@ -0,0 +1,11 @@
+package com.springdemo;
+
+public class CricketCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		// TODO Auto-generated method stub
+		return "Run 1 hr";
+	}
+
+}
